f2ed8a5bf7af9608667fc0dbf300b2b5caf5e07a
amione <57235791+xamionex@users.noreply.github.com>
2024-11-26 06:49:04 +0100

    bump version

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,5 +1,5 @@
 internal static class PluginInfo {
 public const string PluginGUID = "amione.Pupifier";
 public const string PluginName = "Pupifier";
-public const string PluginVersion = "0.1.6";
+public const string PluginVersion = "0.1.7";
 }
